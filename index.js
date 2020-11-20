e1f189ac6a5813799cdbcbd5d32a9389620d0db1
Shashank Pradhan <shashankpradhan911@gmail.com>
2020-11-20 15:28:23 +0530

    nodemailer

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import nodemailer from 'nodemailer';
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'shashankpradhan911@gmail.com',
-        pass: 'xslkfjqtsgcultsf'
+        user: 'yourusername@gmail.com',
+        pass: 'youpass'
     }
 });
 
 var mailOptions = {
-    from: 'shashankpradhan911@gmail.com',
-    to: 'shashankpradhan199@gmail.com',
+    from: 'youruser@gmail.com',
+    to: 'recipent@gmail.com',
     subject: 'Sending Email using Node.js',
     text: 'That was easy!'
 };
